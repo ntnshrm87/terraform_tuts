90bbea5b8a369e9bdff22e51ce6348affb097917
SecOpsCrooner <4hathacker@example.com>
2020-08-07 13:56:39 +0530

    taint and untaint in terraform modules

---
+++ taint-untaint-ex/main.tf
@@ -0,0 +1,13 @@
+module "import-s3" {
+  source = "./module"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# terraform import module.import-s3.aws_s3_bucket.foo import-as-module-1a
+
+# terraform taint aws_s3_bucket.foo
+
+# terraform taint module.import-s3.aws_s3_bucket.foo
