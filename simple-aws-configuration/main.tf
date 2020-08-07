2b8a308c822b8b39bc7d03f5c7b4c29a3de5c7be
SecOpsCrooner <4hathacker@example.com>
2020-08-07 09:18:28 +0530

    modifications to vars...

--- simple-aws-configuration/main.tf
+++ simple-aws-configuration/main.tf
@@ -1,9 +1,6 @@
-resource "aws_s3_bucket" "bucky" {
-  bucket_prefix = "my-tf-test-bucket-"
-  acl           = "private"
+resource "aws_s3_bucket" "bucky1" {
+  bucket_prefix = var.bucket_prefix
+  acl           = var.acl
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+  tags = var.tags
 }
